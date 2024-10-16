beaa5cef00b7a3b8d447c689ed1797aa11c6592e
Jennifer <1001.26480892.ucla@gmail.com>
2024-10-16 08:46:55 -0400

    ajustes, detalles en el footer, navbar y sucursales

---
+++ src/pages/register/register.jsx
@@ -0,0 +1,15 @@
+import "./register.css";
+import Navbar from "../../components/Navbar/Navbar";
+import Footer from "../../components/Footer/Footer";
+
+function Register() {
+  return (
+    <div>
+        <Navbar />
+        
+        <Footer />
+    </div>
+  );
+}
+
+  export default Register;
