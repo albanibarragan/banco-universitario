e262576094b15b072fa7689289216be2c78be2fc
albani <155846628+albanibarragan@users.noreply.github.com>
2024-10-20 18:08:15 -0400

    page login primera parte

--- src/components/Forms/LoginForm/LoginForm.jsx
+++ src/components/Forms/LoginForm/LoginForm.jsx
@@ -1,28 +1,28 @@
-import "./LoginForm.css"
+import "./LoginForm.css";
 
-const LoginForm = () =>{
-    return(
+const LoginForm = () => {
+    return (
         <div className="form-box">
             <h2 className="tittle-login">Iniciar sesión</h2>
             <p className="sub-login">¡Bienvenido, ingrese sus credenciales!</p>
-        <form className="form-login">
-            <div className="input-group">
-                <label> Correo Electronico</label>
-                    <input type="email"/>
-            </div>
-            <div className="input-group">
-                <label >Contraseña</label>
-                <input type="password" name="password" id="password" placeholder="" />
-            </div>
-                <button className="button-login">Ingresar</button>
-            <div className="register-link">
-            <p >¿No tienes cuenta?
-		    <a href="#" >Registrarse</a>
-	        </p>
-            </div>
-        </form>
+            <form className="form-login">
+                <div className="input-group">
+                    <label htmlFor="email">Correo Electrónico</label>
+                    <input type="email" id="email" placeholder="correo@example.com" required />
+                </div>
+                <div className="input-group">
+                    <label htmlFor="password">Contraseña</label>
+                    <input type="password" name="password" id="password" placeholder="********" required />
+                </div>
+                <button className="button-login" type="submit">Ingresar</button>
+                <div className="register-link">
+                    <p>¿No tienes cuenta? 
+                        <a href="#"> Registrarse</a>
+                    </p>
+                </div>
+            </form>
         </div>
-    )
+    );
 }
 
-export default LoginForm
+export default LoginForm;
